ed494478c0a2a8e7e039e8edd205f51db88d7de1
pipiczi <istvan.pipicz@gmail.com>
2019-09-12 19:49:49 +0200

    Deployment and ssh authentication fix

--- .travis/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa.enc # Allow read access to the private key
-ssh-add .travis/id_rsa.enc # Add the private key to SSH
-
-scp client/android/app/build/outputs/apk/release/app-release-unsigned.apk rsptn.ddns.net:/mnt/hdd1/appholder
