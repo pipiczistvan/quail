912e70ec75b6fd9e2b5fd34a58ced36d7112f777
pipiczi <istvan.pipicz@gmail.com>
2019-09-13 07:18:38 +0200

    Deploy script fix

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,5 +6,5 @@ TARGET_DIR=pi@rsptn.ddns.net:/mnt/hdd1/appholder
 
 mkdir $TEMP_DIR
 find $BUILD_DIR -type f -name '*.apk' -exec cp '{}' $TEMP_DIR ';'
-for file in $TEMP_DIR/*.apk; do mv -- $f $TEMP_DIR/$(basename $file .apk)-$(date -r "$f" +"%Y%m%d-%H%M%S").apk; done
+for f in $TEMP_DIR/*.apk; do mv -- "$f" "$TEMP_DIR/$(basename $f .apk)-$(date -r "$f" +"%Y%m%d-%H%M%S").apk"; done
 rsync -r --delete-after --quiet $TEMP_DIR $TARGET_DIR
